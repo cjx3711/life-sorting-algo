3169cc2d3b73aba87e99d2132889d4f09fa71353
Jia Xun <jxun@indeed.com>
2017-07-04 10:37:32 +0900

    Readme updating scripts

---
+++ readme-update.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest https://raw.githubusercontent.com/cjx3711/life-sorting-algo/master/README.md -OutFile README.md
---
+++ readme-update.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -O https://raw.githubusercontent.com/cjx3711/life-sorting-algo/master/README.md
